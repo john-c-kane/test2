c111aa84009d477ae470d63b674880f860d727cb
John Kane <john.c.kane@ibm.com>
2023-08-01 18:17:56 -0400

    test1

---
+++ movie/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse('<h1>Welcome to Home Page</h1>')
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
